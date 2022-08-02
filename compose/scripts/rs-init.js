89e3b1635ac1f2f67a68ccdf9042e13b9801d48e
Us10n <srabinin.v@gmail.com>
2022-08-02 13:55:43 +0300

    Moved docker compose files in seperate folder

---
+++ compose/scripts/rs-init.js
@@ -0,0 +1,13 @@
+// var cfg = {
+//     "_id": "rs",
+//     "version": 1,
+//     "members": [
+//         {
+//             "_id": 0,
+//             "host": "mongo:27017",
+//             "priority": 2
+//         },
+//     ]
+// };
+// rs.initiate(cfg);
+rs.initiate();
